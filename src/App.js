7025353e65297e1378e8945a20f548f96251296b
afifafr <tayeb.afifa@gmail.com>
2023-12-09 00:05:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AddTask from "./components/AddTask";
+import ListTask from "./components/ListTask";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+      <h1>ToDo App</h1>
+      <AddTask />
+      <ListTask />
+    </div>
+  );
+}
+
+export default App;
